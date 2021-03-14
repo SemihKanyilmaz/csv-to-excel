88e645e167d0cdf1847a4af396cf197086b0c528
semihkanyilmaz <semihkanyilmaz@gmail.com>
2021-03-14 10:58:16 +0300

    .

--- main.go
+++ main.go
@@ -3,43 +3,42 @@ package main
 import (
 	"encoding/csv"
 	"errors"
-	"log"
-	"mime/multipart"
-	"net/http"
+	"fmt"
+	"os"
 	"strconv"
 	"strings"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
-	"github.com/labstack/echo"
 )
 
 func main() {
-	e := echo.New()
-	e.HideBanner = true
 
-	e.POST("/", Upload)
+	err := CsvToExcel("samplefile.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	log.Fatal(e.Start(":1923"))
 }
 
-func CsvToExcel(file *multipart.FileHeader) (string, error) {
+func CsvToExcel(fileName string) error {
 
-	fileSplits := strings.Split(file.Filename, ("."))
+	fileSplits := strings.Split(fileName, ("."))
 
 	if !strings.Contains(fileSplits[1], "csv") {
-		return "", errors.New("The extension is not CSV")
+		return errors.New("The extension is not CSV")
 	}
 
 	const sheet string = "Sheet1"
 	var letters = [...]string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
-	src, err := file.Open()
+
+	src, err := os.Open(fileName)
 	if err != nil {
-		return "", errors.New("An error occurred while opening the file")
+		return err
 	}
 	defer src.Close()
 	csvLines, err := csv.NewReader(src).ReadAll()
 	if err != nil {
-		return "", errors.New("An error occurred while reading the file")
+		return err
 	}
 	f := excelize.NewFile()
 	ctr := 1
@@ -56,24 +55,8 @@ func CsvToExcel(file *multipart.FileHeader) (string, error) {
 	}
 	newFileName := fileSplits[0] + ".xlsx"
 	if err := f.SaveAs(newFileName); err != nil {
-		return "", errors.New("An error occurred while creating the file")
+		return err
 	}
-	return newFileName, nil
-}
-
-func Upload(c echo.Context) error {
-
-	f, err := c.FormFile("file")
-
-	if err != nil {
-		return c.NoContent(http.StatusBadRequest)
-	}
-
-	file, err := CsvToExcel(f)
-
-	if err != nil {
-		return c.JSON(500, err.Error())
-	}
-
-	return c.Attachment(file, file)
+	fmt.Printf("%s has been created successfully",newFileName)
+	return nil
 }
